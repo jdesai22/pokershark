f33bcee898614fde6a212be77d571d3cd9b14f4b
Jai Desai <contactjaidesai@gmail.com>
2025-03-08 22:16:39 -0500

    conected stats page to db

--- frontend/screens/StatsScreen.js
+++ frontend/screens/StatsScreen.js
@@ -1,25 +1,38 @@
 // screens/StatsScreen.js
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { Card, Title } from 'react-native-paper';
-import { LineChart } from 'react-native-chart-kit';
-
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Card, Title } from "react-native-paper";
+import { LineChart } from "react-native-chart-kit";
+import { useAuth } from "@/hooks/useAuth";
+import {
+  getPlayerStats,
+  getPlayerMatchHistory,
+} from "@/utils/firestoreQueries";
 const StatsScreen = () => {
-  const chartData = {
-    labels: ['Nov 23', '24', '25', '26', '27', '28', '29', '30'],
-    datasets: [
-      {
-        data: [5, 6, 8, 12, 15, 18, 22, 25],
-        color: (opacity = 1) => `rgba(0, 122, 255, ${opacity})`, // Blue line
-        strokeWidth: 2,
-      },
-    ],
-  };
+  // const chartData = {
+  //   labels: ["Nov 23", "24", "25", "26", "27", "28", "29", "30"],
+  //   datasets: [
+  //     {
+  //       data: [5, 6, 8, 12, 15, 18, 22, 25],
+  //       color: (opacity = 1) => `rgba(0, 122, 255, ${opacity})`, // Blue line
+  //       strokeWidth: 2,
+  //     },
+  //   ],
+  // };
+
+  const { user, isAuthenticated } = useAuth();
+
+  const [winlossRatio, setWinlossRatio] = useState(0);
+  const [foldRatio, setFoldRatio] = useState(0);
+  const [vpip, setVpip] = useState(0);
+
+  const [earnings, setEarnings] = useState([]);
+  const [chartData, setChartData] = useState([]);
 
   const chartConfig = {
-    backgroundColor: '#ffffff',
-    backgroundGradientFrom: '#ffffff',
-    backgroundGradientTo: '#ffffff',
+    backgroundColor: "#ffffff",
+    backgroundGradientFrom: "#ffffff",
+    backgroundGradientTo: "#ffffff",
     decimalPlaces: 0,
     color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
     labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
@@ -27,22 +40,72 @@ const StatsScreen = () => {
       borderRadius: 16,
     },
     propsForDots: {
-      r: '6',
-      strokeWidth: '2',
-      stroke: '#007AFF',
+      r: "6",
+      strokeWidth: "2",
+      stroke: "#007AFF",
     },
   };
+
+  const setupChartData = (earnings) => {
+    // Check if earnings exist and is an array
+    if (!earnings || !Array.isArray(earnings)) {
+      console.log("No earnings data available");
+      return;
+    }
+
+    const labels = earnings.map((_, index) => `${index + 1}`);
+    labels[0] = "Game 1";
+    const data = earnings;
+
+    // Make sure we have data before setting the chart data
+    if (data.length > 0) {
+      setChartData({
+        labels,
+        datasets: [
+          {
+            data,
+            color: (opacity = 1) => `rgba(0, 122, 255, ${opacity})`,
+            strokeWidth: 2,
+          },
+        ],
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      getPlayerStats(user.uid).then((stats) => {
+        // console.log("Stats:", stats);
+        // console.log("Stats earnings:", stats.earnings); // Check the structure of earnings
+        setWinlossRatio(stats.win_loss_ratio);
+        setFoldRatio(stats.fold_ratio);
+        setVpip(stats.vpip);
+        const earnings = stats.earnings;
+        setEarnings(earnings);
+        setupChartData(earnings);
+      });
+      // getPlayerMatchHistory(user.uid).then((matchHistory) => {
+      //   console.log(matchHistory);
+      // });
+    }
+  }, [user]);
+
+  useEffect(() => {
+    console.log(chartData);
+  }, [chartData]);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image
-          source={{ uri: '' }} // Replace with actual profile image URL
+          source={{ uri: "" }} // Replace with actual profile image URL
           style={styles.profileImage}
         />
         <Text style={styles.title}>PokerShark Analytics</Text>
         <Image
-          source={{ uri: 'https://asiaiplaw.com/storage/media/image/article/7eb532aef980c36170c0b4426f082b87/banner/939314105ce8701e67489642ef4d49e8/conversions/Picture1-extra_large.jpg' }} // Replace with actual profile image URL
+          source={{
+            uri: "https://asiaiplaw.com/storage/media/image/article/7eb532aef980c36170c0b4426f082b87/banner/939314105ce8701e67489642ef4d49e8/conversions/Picture1-extra_large.jpg",
+          }} // Replace with actual profile image URL
           style={styles.profileImage}
         />
       </View>
@@ -52,28 +115,32 @@ const StatsScreen = () => {
           <View style={styles.metrics}>
             <View style={styles.metric}>
               <Text style={styles.metricLabel}>Win/Loss Ratio</Text>
-              <Text style={styles.metricValue}>0.683</Text>
+              <Text style={styles.metricValue}>{winlossRatio}</Text>
             </View>
             <View style={styles.metric}>
               <Text style={styles.metricLabel}>Fold Ratio</Text>
-              <Text style={styles.metricValue}>0.52</Text>
+              <Text style={styles.metricValue}>{foldRatio}</Text>
             </View>
             <View style={styles.metric}>
               <Text style={styles.metricLabel}>VPIP</Text>
-              <Text style={styles.metricValue}>21%</Text>
+              <Text style={styles.metricValue}>{vpip}</Text>
             </View>
           </View>
         </Card>
         <Card style={styles.card}>
-          <Title style={styles.cardTitle}>Wins</Title>
-          <LineChart
-            data={chartData}
-            width={350}
-            height={220}
-            chartConfig={chartConfig}
-            bezier
-            style={styles.chart}
-          />
+          <Title style={styles.cardTitle}>Amount Won</Title>
+          {chartData &&
+            chartData.datasets &&
+            chartData.datasets[0].data.length > 0 && (
+              <LineChart
+                data={chartData}
+                width={350}
+                height={220}
+                chartConfig={chartConfig}
+                bezier
+                style={styles.chart}
+              />
+            )}
         </Card>
       </View>
     </View>
@@ -83,20 +150,20 @@ const StatsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingTop: 50,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 20,
     paddingTop: 20,
     marginBottom: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   profileImage: {
     width: 30,
@@ -112,24 +179,24 @@ const styles = StyleSheet.create({
   },
   cardTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     padding: 10,
   },
   metrics: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     paddingVertical: 10,
   },
   metric: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   metricLabel: {
     fontSize: 14,
-    color: '#666',
+    color: "#666",
   },
   metricValue: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   chart: {
     marginVertical: 8,
